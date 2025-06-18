6422f609f572771b0509977a564f7eeda7b7656a
tifpham021 <tpham346@gatech.edu>
2025-06-18 01:09:16 -0400

    created the backend products api, light/dark mode logic, and worked on frontend for the add product page

---
+++ backend/models/product.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true //createdAt, updatedAt fields
+});
+
+const Product = mongoose.model('Product', productSchema); 
+// 'Product' tells mongoose to create this collection
+
+export default Product
